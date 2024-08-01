0118ca28200cda4a0327942446f0cb001f875858
artemFedossv <a.fedossov90@gmail.com>
2024-07-31 23:59:45 -0300

    se termino con los productos

--- src/managers/fileSystem/productsManager.js
+++ src/managers/fileSystem/productsManager.js
@@ -7,25 +7,23 @@ class ProductsManager {
     }
 
     readProducts = async () => {
-       
-            if(fs.existsSync(path)){
-                const data = await fs.promises.readFile(path, 'utf-8');
-                const products = JSON.parse(data);
-                return products;
-            }
-       
-            
-        
+        if (fs.existsSync(path)) {
+            const data = await fs.promises.readFile(path, 'utf-8');
+            const products = JSON.parse(data);
+            return products;
+        }
         return [];
     }
+
     getProducts = async () => {
-       const products = await this.readProducts()
-       return products
+        const products = await this.readProducts()
+        return products
     }
+
     getProduct = async (productId) => {
         const products = await this.readProducts();
         const product = products.find(p => p.id === productId)
-        if(product) {
+        if (product) {
             return product
         }
         else {
@@ -35,18 +33,44 @@ class ProductsManager {
 
     createProduct = async newProduct => {
         try {
-            const products = await this.readProducts()
+            const products = await this.readProducts();
             newProduct.id = Date.now().toString();
             products.push(newProduct);
             await fs.promises.writeFile(path, JSON.stringify(products, null, '\t'));
             return newProduct;
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
 
     }
-    updateProduct = async () => {}
-    deleteProduct = async () => {}
-}    
+
+    updateProduct = async (newProduct, productId) => {
+        try {
+            const products = await this.readProducts();
+            const productIndex = products.findIndex(p => p.id === productId);
+            if (productIndex !== -1) {
+                const update = { ...products[productIndex], ...newProduct };
+                products[productIndex] = update;
+                await fs.promises.writeFile(path, JSON.stringify(products, null, '\t'));
+                return update;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    deleteProduct = async (productId) => {
+        try {
+            let products = await this.readProducts();
+            const productDelete = products.find(p => p.id === productId);
+            products = products.filter(p => p.id !== productId);
+            await fs.promises.writeFile(path, JSON.stringify(products, null, '\t'));
+            return productDelete;
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
 
 module.exports = ProductsManager;
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const ProductsManager = require('../managers/fileSystem/productsManager');
-const { status } = require('express/lib/response');
 
 const router = Router();
 
-const { getProducts, createProduct, getProduct } = new ProductsManager();
+const { getProducts, createProduct, getProduct, updateProduct, deleteProduct } = new ProductsManager();
 
 router.get('/', async (req, res) => {
   try {
@@ -26,14 +25,31 @@ router.get('/:pid', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const { body } = req 
-        const products = await createProduct(body)
-        res.send({ status: 'true', data: products })
+        const { body } = req;
+        const products = await createProduct(body);
+        res.send({ status: 'true', data: products });
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 })
 
+router.put('/:pid', async (req, res) => {
+  try {
+    const { body } = req;
+    const products = await updateProduct(body, req.params.pid);
+    res.send({ status: 'true', data: products });
+  } catch (error) {
+    console.error(error);
+  }
+})
 
+router.delete('/:pid', async (req, res) => {
+  try {
+    const product = await deleteProduct(req.params.pid);
+    res.send({ status: 'true', data: product });
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 module.exports = router;
